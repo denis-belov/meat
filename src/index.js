19fa531bfcbbb58d11b6dba1f80441ab06ac4e61
Denis Belov <dsbelov63@gmail.com>
2021-05-14 11:38:40 -0400

    up

--- src/index.js
+++ src/index.js
@@ -14,10 +14,6 @@ import '@babel/polyfill';
 
 
 
-const dpr = window.devicePixelRatio || 1;
-
-
-
 window.THREE = THREE;
 
 
@@ -34,8 +30,97 @@ gltf_loader.setDRACOLoader(draco_loader);
 
 
 
-let mixer = null;
-const clock = new THREE.Clock();
+// let mixer = null;
+// const clock = new THREE.Clock();
+
+
+
+const loadModel = (scene, file_path) =>
+{
+	return new Promise(
+
+		(resolve) =>
+		{
+			gltf_loader.load(
+
+				file_path,
+
+				async (gltf) =>
+				{
+					// mixer = new THREE.AnimationMixer(gltf.scene);
+					// // alert(gltf.animations.length);
+					// const action = mixer.clipAction(gltf.animations[0]);
+
+					// const CubeTextureLoader = new THREE.CubeTextureLoader();
+					// CubeTextureLoader.setPath('textures/cubemap/');
+
+					// const cube_map =
+					// 	await CubeTextureLoader.load([ 'posx.jpg', 'negx.jpg', 'posy.jpg', 'negy.jpg', 'posz.jpg', 'negz.jpg' ]);
+
+					// // alert(cube_map);
+
+					// gltf.scene.traverse((elm) => {
+
+					// 	if (elm.isMesh) {
+
+					// 		if (Array.isArray(elm.material)) {
+
+					// 			elm.material.forEach(
+
+					// 				(_elm) => {
+
+					// 					_elm.envMap = cube_map;
+					// 					// _elm.side = THREE.FrontSide;
+					// 					_elm.needsUpdate = true;
+					// 				},
+					// 			);
+					// 		}
+					// 		else {
+
+					// 			elm.material.envMap = cube_map;
+					// 			// elm.material.side = THREE.FrontSide;
+					// 			elm.material.needsUpdate = true;
+					// 		}
+					// 	}
+					// });
+
+					scene.add(gltf.scene);
+
+					gltf.scene.position.z = -2;
+
+					resolve();
+
+					// const animate = () => {
+
+					// 	mixer && mixer.update(clock.getDelta());
+
+					// 	requestAnimationFrame(animate);
+					// };
+
+					// animate();
+
+					// action.play();
+
+					// gltf.animations; // Array<THREE.AnimationClip>
+					// gltf.scene; // THREE.Group
+					// gltf.scenes; // Array<THREE.Group>
+					// gltf.cameras; // Array<THREE.Camera>
+					// gltf.asset; // Object
+				},
+
+				// (xhr) =>
+				// {
+				// 	console.log((xhr.loaded / xhr.total * 100) + '% loaded');
+				// },
+
+				// (error) =>
+				// {
+				// 	alert('An error happened');
+				// },
+			);
+		},
+	);
+}
 
 
 
@@ -68,67 +153,27 @@ window.addEventListener(
 
 								process:
 
-									() => {
-
-										alert('image found');
+									async () => {
 
 										XR8.XrController.configure({ imageTargets: [] });
 
+										alert('image found');
+
 										document.getElementById('spinner').style.display = 'block';
 
-										const { scene, camera } = XR8.Threejs.xrScene();
+										const { scene } = XR8.Threejs.xrScene();
 
-										// Sync the xr controller's 6DoF position and camera paremeters with our scene.
-										XR8.XrController
-											.updateCameraProjectionMatrix({ origin: camera.position, facing: camera.quaternion });
+										await loadModel(
 
-										gltf_loader.load(
+											scene,
 
-											// 'models/Butcher\'s_Word_ Factory.glb',
-											'models/Grill.glb',
-
-											(gltf) => {
-
-												// LOG(123);
-
-												document.getElementById('spinner').style.display = 'none';
-
-												mixer = new THREE.AnimationMixer(gltf.scene);
-												// alert(gltf.animations.length);
-												const action = mixer.clipAction(gltf.animations[0]);
-
-												scene.add(gltf.scene);
-
-												gltf.scene.position.z = -5;
-
-												const animate = () => {
-
-													mixer && mixer.update(clock.getDelta());
-
-													requestAnimationFrame(animate);
-												};
-
-												animate();
-
-												action.play();
-
-												// gltf.animations; // Array<THREE.AnimationClip>
-												// gltf.scene; // THREE.Group
-												// gltf.scenes; // Array<THREE.Group>
-												// gltf.cameras; // Array<THREE.Camera>
-												// gltf.asset; // Object
-											},
-
-											// (xhr) => {
-
-											// 	console.log((xhr.loaded / xhr.total * 100) + '% loaded');
-											// },
-
-											// (error) => {
-
-											// 	alert('An error happened');
-											// },
+											'models/Butcher\'s_Word_ Factory.glb',
+											// 'models/Grill.glb',
 										);
+
+										await loadModel(scene, 'models/Meatman.glb');
+
+										document.getElementById('spinner').style.display = 'none';
 									},
 							},
 							// {event: 'reality.imageupdated', process: logEvent},
@@ -142,43 +187,50 @@ window.addEventListener(
 
 					onStart: () => {
 
-						const { scene, renderer } = XR8.Threejs.xrScene();
-
-						LOG(XR8.Threejs.xrScene());
-
-						const gl = renderer.getContext();
-
-						gl.viewport(0, 0, window.innerWidth * dpr, window.innerHeight * dpr);
+						const { renderer, camera, scene } = XR8.Threejs.xrScene();
 
 						renderer.setSize(window.innerWidth, window.innerHeight);
 
-						canvas.width = window.innerWidth;
-						canvas.height = window.innerHeight;
-						canvas.style.width = '100%';
-						canvas.style.height = '100%';
+						renderer.outputEncoding = THREE.sRGBEncoding;
 
-						const ambient_light = new THREE.AmbientLight(0xFFFFFF);
+						// Sync the xr controller's 6DoF position and camera paremeters with our scene.
+						XR8.XrController.updateCameraProjectionMatrix(
+
+							{
+								cam: {
+									pixelRectWidth: window.innerWidth,
+									pixelRectHeight: window.innerHeight,
+									nearClipPlane: 0.1,
+									farClipPlane: 100,
+								},
+
+								origin: camera.position,
+								facing: camera.quaternion,
+							},
+						);
+
+						const ambient_light = new THREE.AmbientLight(0xFFFFFF, 1);
 
 						scene.add(ambient_light);
 
-						const spot_light = new THREE.SpotLight(0xFFFFFF);
-						spot_light.intensity = 2;
-						spot_light.distance = 0;
-						spot_light.penumbra = 1;
-						spot_light.decay = 2;
-						spot_light.angle = Math.PI * 0.5;
-						spot_light.position.set(0, 10, 0);
+						// const spot_light = new THREE.SpotLight(0xFFFFFF);
+						// spot_light.intensity = 2;
+						// spot_light.distance = 0;
+						// spot_light.penumbra = 1;
+						// spot_light.decay = 2;
+						// spot_light.angle = Math.PI * 0.5;
+						// spot_light.position.set(0, 10, 0);
 
-						scene.add(spot_light);
-						scene.add(spot_light.target);
+						// scene.add(spot_light);
+						// scene.add(spot_light.target);
 
 						// Prevent scroll/pinch gestures on canvas.
-						canvas.addEventListener(
+						// canvas.addEventListener(
 
-							'touchmove',
+						// 	'touchmove',
 
-							(evt) => evt.preventDefault(),
-						);
+						// 	(evt) => evt.preventDefault(),
+						// );
 
 						// Recenter content when the canvas is tapped.
 						canvas.addEventListener(
@@ -196,6 +248,8 @@ window.addEventListener(
 				},
 			],
 		);
+
+		// XR8.XrController.configure({ disableWorldTracking: true });
 
 		XR8.run({ canvas });
 	},
